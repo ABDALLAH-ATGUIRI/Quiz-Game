64f34cf18d693ef80336a7c0b137f79edaa1b8cb
ABDALLAH ATGUIRI <abdallahatguiri@gmail.com>
2023-04-08 03:40:38 +0000

    Update Background.jsx and Button.jsx

--- quiz_game/src/components/Background.jsx
+++ quiz_game/src/components/Background.jsx
@@ -5,7 +5,7 @@ export const Background = ({ children }) => {
   return (
 
     <Fragment>
-      <div className="flex flex-col w-screen px-5 h-full bg-[#1A1A1A] items-center " style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: "cover" }}>
+      <div className="flex flex-col w-screen px-5 h-screen bg-[#1A1A1A] items-center " style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: "cover" }}>
         {children}
       </div>
     </Fragment>)
